047b28278670cc653f321105979544f834288da7
pajomi <ptrckmchl@gmail.com>
2018-09-15 11:00:58 +0200

    Add files via upload

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['protocol']    = 'smtp';
+$config['mailpath']    = '/usr/sbin/sendmail';
+$config['smtp_host']   = 'smtp.strato.de';
+$config['smtp_user']   = 'info@tank-mops.de';
+$config['smtp_pass']   = 'bruchstr38';
+$config['smtp_port']   = '587';
+$config['smtp_crypto'] = 'tls';
+$config['mailtype']    = 'html';
+$config['charset']     = 'utf-8';
